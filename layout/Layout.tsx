a67836ac39638d0cd44ef416c9a5f0f781e6e2ab
samclassix <samclassix@proton.me>
2025-02-01 18:02:04 +0100

    feat: intro pwa template

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -16,6 +16,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 		<>
 			<Head>
 				<title>WrytLabs - Home</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+				<meta name="theme-color" content="#000000" />
+				<link rel="manifest" href="/manifest.json" />
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 			</Head>
 
 			{isHome ? (
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+	disable: false,
+	disableDevLogs: false,
+});
+
 const nextConfig = {
 	reactStrictMode: true,
 	transpilePackages: ['@wrtylabs/api'],

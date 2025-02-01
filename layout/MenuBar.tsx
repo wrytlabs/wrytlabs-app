66a74a6efd65a91afc4be72c1e5fb581b8c5678d
samclassix <samclassix@proton.me>
2025-02-01 12:55:17 +0100

    feat: layout for home or clear page, mobile layout adjusted

--- layout/MenuBar.tsx
+++ layout/MenuBar.tsx
@@ -7,10 +7,10 @@ export default function MenuBar() {
 
 	return (
 		<>
-			<div className="fixed top-0 left-0 right-0 z-10 m-4 md:m-6 md:mx-10 xl:mx-20">
-				<div className="backdrop-blur bg-card-body-primary border-b-2 border-menu-separator rounded-3xl shadow-lg py-4 px-6 xl:px-10 flex items-center">
+			<div className="fixed top-0 left-0 right-0 z-10 mx-2 my-4 md:m-6 md:mx-10 xl:mx-20">
+				<div className="backdrop-blur bg-card-body-primary border-b-2 border-menu-separator rounded-3xl shadow-lg px-6 xl:px-10 flex items-center">
 					<div className="flex items-center space-x-4 w-full">
-						<div onClick={() => setIsNavBarOpen(!isNavBarOpen)} className={`cursor-pointer`}>
+						<div onClick={() => setIsNavBarOpen(!isNavBarOpen)} className={`cursor-pointer md:p-5 p-3`}>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"

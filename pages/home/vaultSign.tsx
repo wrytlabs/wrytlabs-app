8c16358ffac294e53db68d994885de8d7c2e5f4b
samclassix <samclassix@proton.me>
2025-07-15 12:04:54 +0200

    feat: variable nonce

--- pages/home/vaultSign.tsx
+++ pages/home/vaultSign.tsx
@@ -3,48 +3,51 @@ import { useSignTypedData } from 'wagmi';
 
 import AppTitle from '@components/AppTitle';
 import { SOCIAL } from '@utils';
-import { useEffect } from 'react';
-
-const dataToSign = {
-	domain: {
-		verifyingContract: '0xeb4af6fa3afa08b10d593ec8ff87efb03bc04645',
-		chainId: BigInt(1),
-	},
-	types: {
-		EIP712Domain: [
-			{ name: 'chainId', type: 'uint256' },
-			{ name: 'verifyingContract', type: 'address' },
-		],
-		SafeTx: [
-			{ type: 'address', name: 'to' },
-			{ type: 'uint256', name: 'value' },
-			{ type: 'bytes', name: 'data' },
-			{ type: 'uint8', name: 'operation' },
-			{ type: 'uint256', name: 'safeTxGas' },
-			{ type: 'uint256', name: 'baseGas' },
-			{ type: 'uint256', name: 'gasPrice' },
-			{ type: 'address', name: 'gasToken' },
-			{ type: 'address', name: 'refundReceiver' },
-			{ type: 'uint256', name: 'nonce' },
-		],
-	},
-	primaryType: 'SafeTx',
-	message: {
-		to: '0x3c124fb80e6eedd88d108c2f04013ae5cd571abc',
-		value: BigInt(0),
-		data: '0xa9059cbb000000000000000000000000201051ae0fddac0ce47b299e4673caa39f32a96100000000000000000000000000000000000000000000001ea50759d411afcf79',
-		operation: 0,
-		baseGas: BigInt(0),
-		gasPrice: BigInt(0),
-		gasToken: '0x0000000000000000000000000000000000000000',
-		refundReceiver: '0x0000000000000000000000000000000000000000',
-		nonce: BigInt(1355),
-		safeTxGas: BigInt(0),
-	},
-} as const;
+import { useEffect, useState } from 'react';
+import NormalInput from '@components/Input/NormalInput';
+import AddressInput from '@components/Input/AddressInput';
 
 export default function PageHome() {
+	const [nonce, setNonce] = useState(1062);
 	const { data, error, signTypedData } = useSignTypedData();
+
+	const dataToSign = {
+		domain: {
+			verifyingContract: '0xeb4af6fa3afa08b10d593ec8ff87efb03bc04645',
+			chainId: BigInt(1),
+		},
+		types: {
+			EIP712Domain: [
+				{ name: 'chainId', type: 'uint256' },
+				{ name: 'verifyingContract', type: 'address' },
+			],
+			SafeTx: [
+				{ type: 'address', name: 'to' },
+				{ type: 'uint256', name: 'value' },
+				{ type: 'bytes', name: 'data' },
+				{ type: 'uint8', name: 'operation' },
+				{ type: 'uint256', name: 'safeTxGas' },
+				{ type: 'uint256', name: 'baseGas' },
+				{ type: 'uint256', name: 'gasPrice' },
+				{ type: 'address', name: 'gasToken' },
+				{ type: 'address', name: 'refundReceiver' },
+				{ type: 'uint256', name: 'nonce' },
+			],
+		},
+		primaryType: 'SafeTx',
+		message: {
+			to: '0x3c124fb80e6eedd88d108c2f04013ae5cd571abc',
+			value: BigInt(0),
+			data: '0xa9059cbb000000000000000000000000201051ae0fddac0ce47b299e4673caa39f32a96100000000000000000000000000000000000000000000001ea50759d411afcf79',
+			operation: 0,
+			baseGas: BigInt(0),
+			gasPrice: BigInt(0),
+			gasToken: '0x0000000000000000000000000000000000000000',
+			refundReceiver: '0x0000000000000000000000000000000000000000',
+			nonce: BigInt(String(nonce)),
+			safeTxGas: BigInt(0),
+		},
+	} as const;
 
 	const handleSign = async () => {
 		signTypedData(dataToSign);
@@ -86,7 +89,8 @@ export default function PageHome() {
 	return (
 		<AppTitle title="Welcome to WRYT labs">
 			<div className="flex flex-col gap-8 mt-8">
-				<div>
+				<div className="max-w-[30rem]">
+					<AddressInput value={String(nonce)} onChange={(e) => setNonce(parseInt(e))} />
 					<button onClick={handleSign}>Sign Payload</button>
 					{data && <p>Signature: {data}</p>}
 					{error && <p style={{ color: 'red' }}>{error.message}</p>}
